2a5902ea8546471083416a3b7aa71c493706a0f7
MadByAd <adityaaw84@gmail.com>
2024-05-18 19:26:21 +0700

    finish

--- src/LongPoll.js
+++ src/LongPoll.js
@@ -2,7 +2,7 @@
 class LongPoll {
 
     /**
-     * Send a data to a http server
+     * Poll a message from a http server
      *
      * @param {String}   url     The url
      * @param {Object}   data    The data to send
@@ -12,7 +12,7 @@ class LongPoll {
      *                           achieve real time communication
      */
 
-    static send(url, data, handler, repeat = true) {
+    static poll(url, data, handler, repeat = true) {
 
         let form = new FormData;
 
@@ -28,6 +28,35 @@ class LongPoll {
                 if(repeat) {
                     LongPoll.send(url, data, handler, repeat);
                 }
+            }
+        };
+
+        xhttp.open("POST", url);
+        xhttp.send(form);
+
+    }
+
+    /**
+     * Send a message to a http server
+     *
+     * @param {String}   url     The url
+     * @param {Object}   data    The data to send
+     * @param {Function} handler The function to handle the response
+     */
+
+    static poll(url, data, handler) {
+
+        let form = new FormData;
+
+        for(let variable in data) {
+            form.append(variable, data[variable]);
+        }
+
+        let xhttp = new XMLHttpRequest;
+
+        xhttp.onreadystatechange = function() {
+            if(this.readyState == 4 && this.status == 200) {
+                handler(this.responseText);
             }
         };
 
